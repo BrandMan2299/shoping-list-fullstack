cd5fed64f921e13020d5e251647cc8b0fbcaeeb4
MrMoshkovitz <gal@gmail.com>
2020-08-11 18:15:40 +0300

    affecting input and buttons together

--- frontend/product.js
+++ frontend/product.js
@@ -23,7 +23,7 @@ Product.prototype.addToHtml = function () {
   productPrice.className = "productPrice";
   productPrice.innerHTML = `Price: <span class="productTitlePrice">${this.price}</span> NIS`;
 
-  imgSrc = `css/images/${this.name}.jpg`;
+  var imgSrc = `css/images/${this.name}.jpg`;
   imgSrc = checkImage(imgSrc) ? imgSrc : defaultImgSrc;
   var productIMG = document.createElement("div");
   productIMG.className = "image";
@@ -31,12 +31,11 @@ Product.prototype.addToHtml = function () {
 
   var quantity = document.createElement("div");
   quantity.className = "quantity";
-  quantity.innerHTML = `Amount: <input type="text" value=${this.amount} class="amountInput">`;
+  quantity.innerHTML = `<input type="text" value=${this.amount} class="amountInput">`;
 
   var productPriceDiv = document.createElement("div");
   productPriceDiv.className = "productPriceDiv";
-  productPriceDiv.innerHTML = `Total Price: <span class="productTotalPrice">0</span> NIS`;
-
+  productPriceDiv.innerHTML = `Total Price: <span class="productTotalPrice">${this.price * this.amount}</span> NIS`;
 
   var buttons = document.createElement("div");
   buttons.className = "buttons";
@@ -70,7 +69,11 @@ Product.prototype.addToHtml = function () {
   editButton.name = "button";
   editButton.type = "button";
   editButton.className ="edit-button";
-  editButton.innerHTML = `Edit Button: ${editIcon}`;
+  editButton.innerHTML = `Edit${editIcon}`;
+
+  editButton.onclick = () => {
+    editButton.edit(input, name)
+  }
 
   buttons.appendChild(editButton);
   buttons.appendChild(deleteButton);
@@ -106,46 +109,3 @@ function checkImage(image_url) {
     console.log("Image Error", e);
   }
 }
-
-// Product.prototype.addToHtml = function () {
-//   var newBox = document.createElement("div");
-//   newBox.className = "col-5 border border-grey mx-2 productBox";
-//   newBox.setAttribute("id", `product${this.id}`)
-//   this.parent.appendChild(newBox);
-
-//   newBox.innerHTML += "<h2>" + this.name + "</h2>";
-//   newBox.innerHTML += "<div>Price:" + this.price + " nis</div>";
-
-//   imgSrc = `css/images/${this.name}.jpg`;
-//   imgSrc = checkImage(imgSrc) ? imgSrc:defaultImgSrc
-//   newBox.innerHTML += `<img src=${imgSrc} <img>`
-
-//   newBox.innerHTML +=  `<div class="quantity">Amount: <input type="text" value="${this.amount}"</div>`;
-
-//   var buttons = document.createElement("div");
-//   buttons.className = "buttons";
-//   buttons.innerHTML += `<span class="delete-btn"><ion-icon name="trash-outline"></ion-icon></span>`
-//   buttons.innerHTML += `<button name="button" type="button"> <ion-icon name="remove-circle-outline"></ion-icon></button>`
-
-//   newBox.appendChild(buttons);
-
-//   var quantity = document.createElement("div");
-//   quantity.className = "quantity";
-//   quantity.innerHTML = `<input type="text" value="${this.amount}"</input?`
-//   newBox.appendChild(quantity);
-
-// };
-
-// function checkImage(image_url) {
-//     var xhr = new XMLHttpRequest();
-//     try{
-//       xhr.open("HEAD", image_url, false);
-//       xhr.send();
-
-//       console.log(xhr.status);
-//       return xhr.status != 404;
-//     } catch (e) {
-//       console.log(e)
-
-//     }
-//   }
--- frontend/script.js
+++ frontend/script.js
@@ -9,11 +9,18 @@ const add_btn = document.getElementById("add_btn");
 const id_name = document.getElementById("id_name");
 const id_price = document.getElementById("id_price");
 const id_amount = document.getElementById("id_amount");
+// const localStorage = window.localStorage;
+
+// // tasks counter deceleration
+// let productCounter = localStorage.getItem("counter")
+//   ? localStorage.getItem("counter")
+//   : 0;
 
 NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
 HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
 
 var products = [];
+
 var deletedProducts = [];
 
 const getList = async () => {
@@ -28,15 +35,16 @@ const getList = async () => {
   }
 };
 
-window.onload = function () {
+const init = () => {
   showProd().then(() => {
+    // AddToStorage()
     var id_parent = document.getElementById("id_parent");
     var boxes = id_parent.getElementsByClassName("productBox"); // HTML Collection
     Array.from(boxes).forEach((box) => {
       addBTNEvent(box);
     });
   });
-  add_btn.onclick = function () {
+  add_btn.onclick = () => {
     var obj = {
       name: id_name.value,
       id: generateID(),
@@ -44,6 +52,11 @@ window.onload = function () {
       amount: id_amount.value,
     };
     pushProduct(obj);
+    console.log("add to storage?")
+    // AddToStorage(obj)
+    
+
+
   };
 };
 
@@ -68,7 +81,7 @@ const pushProduct = async (product) => {
   console.log(product);
   console.log("Pushed");
   await axios.post("/product", product).then((r) => r.data);
-  showProd();
+  init();
 };
 
 const generateID = () => {
@@ -81,95 +94,112 @@ const generateID = () => {
   return prodId.toString();
 };
 
-function addBTNEvent (box) {
-  console.log("Product Box", box)
-  var buttonsElement = box.querySelector('.buttons')
+function addBTNEvent(box) {
+  console.log("Product Box", box);
+  var buttonsElement = box.querySelector(".buttons");
 
-  var minusBTN = buttonsElement.querySelector('.minus-button')
-  
-  var plusBTN = buttonsElement.querySelector('.plus-button')
-  var editBTN = buttonsElement.querySelector('.edit-button')
+  var minusBTN = buttonsElement.querySelector(".minus-button");
 
+  var plusBTN = buttonsElement.querySelector(".plus-button");
+  var editBTN = buttonsElement.querySelector(".edit-button");
 
-  var deleteBTN = buttonsElement.querySelector('.delete-btn')
+  var deleteBTN = buttonsElement.querySelector(".delete-btn");
 
+  var likeBTN = buttonsElement.querySelector(".like-btn");
+  var prodAmount = box.querySelector(".amountInput");
+  var prodPrice = box.querySelector(".productPrice");
+  var productTitlePrice = prodPrice.querySelector(".productTitlePrice");
+  var productPriceDiv = box.querySelector(".productPriceDiv");
+  var productTotalPrice = productPriceDiv.querySelector(".productTotalPrice");
+  var productName = box.querySelector(".productHeader");
 
-  var likeBTN = buttonsElement.querySelector('.like-btn')
-  var prodAmount = box.querySelector('.amountInput');
-  var prodPrice = box.querySelector('.productPrice')
-  var productTitlePrice = prodPrice.querySelector('.productTitlePrice')
-  var productPriceDiv = box.querySelector('.productPriceDiv')
-  var productTotalPrice = productPriceDiv.querySelector('.productTotalPrice')
-  var productName = box.querySelector('.productHeader')
+  if (!prodAmount.value) {prodAmount.value = 0};
 
+  prodAmount.addEventListener('input', () => {
+    let quantityNum = parseInt(prodAmount.value, 10);
+    let prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
+    let total = prodPriceVal * quantityNum
+    productTotalPrice.innerHTML = total ? total:0;
 
+  });
 
   plusBTN.onclick = () => {
     var quantityNum = parseInt(prodAmount.value, 10);
-    var prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
-    
-    var productTotalPriceVal = productTotalPrice.innerHTML
-    quantityNum += 1
-    prodAmount.value = quantityNum
-    
-    
+    let prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
 
-    productTotalPrice.innerHTML = prodPriceVal * quantityNum
-    console.log("prodAmount Value", prodAmount.value)
-    console.log("FullPrice", productTotalPriceVal)
-
+    quantityNum += 1;
+    prodAmount.value = quantityNum;
+    let total = prodPriceVal * quantityNum
+    productTotalPrice.innerHTML = total ? total:0;
+    console.log("Prod Amount", quantityNum)
   }
-
   minusBTN.onclick = () => {
     var quantityNum = parseInt(prodAmount.value, 10);
-    var prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
+    let prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
     
-    var productTotalPriceVal = productTotalPrice.innerHTML
-    quantityNum -= 1
-    prodAmount.value = quantityNum
-    
-    
-
-    productTotalPrice.innerHTML = prodPriceVal * quantityNum
-    console.log("prodAmount Value", prodAmount.value)
-    console.log("FullPrice", productTotalPriceVal)
-
+    quantityNum -= 1;
+    quantityNum = (quantityNum > 0) ? quantityNum:0;
+    prodAmount.value = (quantityNum > 0) ? quantityNum:0;
+    let total = prodPriceVal * quantityNum
+    productTotalPrice.innerHTML = (total > 0) ? total:0;
+    console.log("Prod Amount", quantityNum)
   }
+  
+  // minusBTN.onclick = () => {
+  //   var quantityNum = parseInt(prodAmount.value, 10);
+  //   var prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
 
-  deleteBTN.onclick =  () => {
-    console.log(products)
-    var productId
-    prod = products.filter(product => product.name == productName.innerText);
-    console.log(prod)
-    productId = prod[0].id
-    console.log(prod[0].id)
+  //   var productTotalPriceVal = productTotalPrice.innerHTML;
+  //   quantityNum -= 1;
+  //   prodAmount.value = quantityNum;
+
+  //   productTotalPrice.innerHTML = prodPriceVal * quantityNum;
+  //   console.log("prodAmount Value", prodAmount.value);
+  //   console.log("FullPrice", productTotalPriceVal);
+  // };
+
+  deleteBTN.onclick = () => {
+    console.log(products);
+    var productId;
+    prod = products.filter((product) => product.name == productName.innerText);
+    console.log(prod);
+    productId = prod[0].id;
+    console.log(prod[0].id);
     // products.forEach( async (product) => {
     //   if (product.name == productName.innerText) {
     //     console.log(product.name, "IS Equal", productName.innerText)
     //     productId = product.id;
     //   }
-      
+
     // })
 
     deleteProduct(productId);
-    window.location.reload()
-console.log(a)
+    init();
+    console.log(a);
     // deleteProduct()
+  };
+  editBTN.onclick = () => {
+    prod = products.filter((product) => product.name == productName.innerText);
+    editProduct(prod);
 
   }
 }
-
-
-
 
 const deleteProduct = async (productId) => {
   console.log(productId);
   await axios.delete(`/products/${productId}`).then((r) => r.data);
   console.log("Deleted");
-
 };
 
 
+const editProduct = async (product) => {
+  console.log(product);
+}
+//   await axios.put(`/products/${productId}`).then((r) => r.data);
+//   console.log("Deleted");
+// };
+
+// Put From Backend
 // app.put("/products/:id", (req, res) => {
 //   products.forEach((product, index) => {
 //     if (product.id === req.params.id) {
@@ -178,3 +208,36 @@ const deleteProduct = async (productId) => {
 //     }
 //   });
 // });
+
+// //Add To Local Storage
+// const AddToStorage = (product) =>{
+//   var target = "productsStorage"
+//   let local = localStorage.getItem(product);
+//   if (local == null) {
+//     local = ""
+//   }
+//   local += product
+//   localStorage.setItem(target, local);
+// }
+// //Get From Local Storage
+// const getStorage = () => {
+//   console.log("getStorage");
+//   var productsStorage = localStorage.getItem("productsStorage");
+//   if (!productsStorage) {
+//     setStorage(products);
+//   }
+//   return productsStorage;
+// };
+// const setStorage = (products) => {
+//   //Set From Local Storage
+//   console.log("setStorage");
+//   var productsString = JSON.stringify(products);
+//   localStorage.setItem("productsStorage", productsString);
+// };
+
+
+
+
+
+
+
