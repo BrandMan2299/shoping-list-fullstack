ecf7d45efe30598de7c42a613909a2d84819b14f
MrMoshkovitz <gal@gmail.com>
2020-08-12 10:51:00 +0300

    Moving Backend JS and Import Port

---
+++ backend/backend.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const { port } = require("./port");
+app.use(express.json());
+
+let products = [
+    {
+        id:'0',
+        name: 'milk',
+        price: '10'
+    },
+    {
+        id:'1',
+        name: 'cerial',
+        price: '23'
+    },
+    {
+        id:'2',
+        name: 'watermellon',
+        price: '29'
+    },
+    {
+        id:'3',
+        name: 'cola',
+        price: '12'
+    },
+    {
+        id:'4',
+        name: 'bread',
+        price: '8'
+    }
+];
+
+app.get('/products', (req, res) => {
+    res.send(products);
+})
+
+app.get('/products/:id', (req, res) => {
+    for (const product of products) {
+        if(product.id === req.params.id)
+        {
+            res.send(product);
+        }
+    }
+})
+
+app.post('/products', (req, res) => {
+    products.forEach(product => {
+        if(product.id === req.body.id){
+            res.send("Id alredy exists, please change id");
+        }
+    })
+    products.push(req.body);
+    res.send(req.body);
+})
+
+app.put('/products/:id', (req, res) => {
+    products.forEach((product ,index) => {
+        if(product.id === req.params.id){
+            products.splice(index, 1, req.body);
+            res.send(req.body);
+        }
+    });
+})
+
+app.delete('/products/:id', (req, res) => {
+    products.forEach((product ,index) => {
+        if(product.id === req.params.id){
+            products.splice(index, 1);
+            res.send('Product deleted')
+        }
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Server Is Listening On Port ${port}`);
+  });
