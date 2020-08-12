8ec17dd7170ddc9dca7a60683adc5bc0626bafb7
MrMoshkovitz <gal@gmail.com>
2020-08-12 11:19:07 +0300

    Show Product ON Html
    Adding Function To Show Product On HTML =

--- frontend/script.js
+++ frontend/script.js
@@ -2,12 +2,10 @@
 // Need To Add Import Port
 port = 3030;
 
-
 // Defining Axios Defaults
 axios.defaults.baseURL = `http://localhost:${port}`;
 axios.defaults.headers.post["Content-Type"] = "application/json; charset=utf-8";
 axios.defaults.validateStatus = (status) => status >= 200 && status < 300;
-
 
 //Global Scope Variables
 const defaultImgSrc = `css/images/nophoto.jpg`;
@@ -16,23 +14,40 @@ const id_name = document.getElementById("id_name");
 const id_price = document.getElementById("id_price");
 const id_amount = document.getElementById("id_amount");
 
-
 // Adding This Allows To use HTML Collection or Node List as an Array
 NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
 HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
 
 var products = [];
 
-
 // Function To Get The Product List From The Server
 const getList = async () => {
-    console.log("getList");
-    try {
-      const { data } = await axios.get(`/products`);
-      console.log("Products Data", data);
-      return data;
-    } catch (e) {
-      console.log("Error Getting Products");
-      console.log(e);
-    }
-  };
+  console.log("getList");
+  try {
+    const { data } = await axios.get(`/products`);
+    console.log("Products Data", data);
+    return data;
+  } catch (e) {
+    console.log("Error Getting Products");
+    console.log(e);
+  }
+};
+
+
+// Function To Show The Products On The HTML Page
+const showProd = async () => {
+  prod_ar = await getList();
+  products = prod_ar;
+  id_parent.innerHTML = "";
+
+  products.forEach((item) => {
+    var newProd = new Product(
+      item.id,
+      item.name,
+      item.price,
+      item.amount,
+      id_parent
+    );
+    newProd.addToHtml();
+  });
+};
