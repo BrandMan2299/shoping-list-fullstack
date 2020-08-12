39903d381606ae71f34f67bee9871d5e6577d211
MrMoshkovitz <gal@gmail.com>
2020-08-12 10:59:03 +0300

    Fixing Backend Push Product
    Changed Post to products instead of products.
    
    removed condition , condition checked on client side.

--- backend/backend.js
+++ backend/backend.js
@@ -44,30 +44,26 @@ let products = [
     res.sendFile("home.html", { root: path.join(__dirname, "../frontend") });
     // res.sendFile(`./../frontend/home.html`);
   });
+
+
   
-
-app.get('/products', (req, res) => {
+  app.get("/products", (req, res) => {
     res.send(products);
-})
+  });
 
-app.get('/products/:id', (req, res) => {
+  app.get("/products/:id", (req, res) => {
     for (const product of products) {
-        if(product.id === req.params.id)
-        {
-            res.send(product);
-        }
+      if (product.id === req.params.id) {
+        res.send(product);
+      }
     }
-})
+  });
 
-app.post('/products', (req, res) => {
-    products.forEach(product => {
-        if(product.id === req.body.id){
-            res.send("Id alredy exists, please change id");
-        }
-    })
+  app.post("/product", (req, res) => {
     products.push(req.body);
+    console.log("Product", req.body);
     res.send(req.body);
-})
+  });
 
 app.put('/products/:id', (req, res) => {
     products.forEach((product ,index) => {
