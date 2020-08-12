5875423127e728e05d5b2bbbb31854e75d5cc10a
MrMoshkovitz <gal@gmail.com>
2020-08-12 11:36:51 +0300

    Edit Button Event Listener
    Edit Button Event Listener Need To Update And Improve

--- frontend/script.js
+++ frontend/script.js
@@ -58,7 +58,7 @@ const showProd = async () => {
 
 //Function To Generate Random ID That's not existing on The Product List
 const generateID = () => {
-    console.log("Generating ID .....")
+  console.log("Generating ID .....");
   let prodId = Math.floor(Math.random() * 1000);
   products.map((product) => {
     if (product.id === prodId) {
@@ -97,54 +97,101 @@ function addBTNEvent(box) {
   if (!prodAmount.value) {
     prodAmount.value = 0;
   }
+
+  //Event Listener On Changing The Amount of Product Using The Input
+  prodAmount.addEventListener("input", () => {
+    let quantityNum = parseInt(prodAmount.value, 10);
+    let prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
+    let total = prodPriceVal * quantityNum;
+    productTotalPrice.innerHTML = total ? total : 0;
+  });
+
+  //Event Listener On Plus Button To Add 1 Product  to amount and update the total price
+  plusBTN.onclick = () => {
+    var quantityNum = parseInt(prodAmount.value, 10);
+    let prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
+
+    quantityNum += 1;
+    prodAmount.value = quantityNum;
+    let total = prodPriceVal * quantityNum;
+    productTotalPrice.innerHTML = total ? total : 0;
+    console.log("Prod Amount", quantityNum);
+  };
+
+  //Event Listener On Minus Button To decrease 1 Product  from the  amount and update the total price
+  minusBTN.onclick = () => {
+    var quantityNum = parseInt(prodAmount.value, 10);
+    let prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
+
+    quantityNum -= 1;
+    quantityNum = quantityNum > 0 ? quantityNum : 0;
+    prodAmount.value = quantityNum > 0 ? quantityNum : 0;
+    let total = prodPriceVal * quantityNum;
+    productTotalPrice.innerHTML = total > 0 ? total : 0;
+  };
+
+  //Event Listener On Delete Button To Delete 1 Product  from the  Products List and HTML
+  deleteBTN.onclick = () => {
+    var productId;
+    prod = products.filter((product) => product.name == productName.innerText);
+    productId = prod[0].id;
+
+    deleteProduct(productId);
+    init(); // Load All Page Func Again
+  };
+
+  //Event Listener On Edit Button To Edit Existing Product
+  editBTN.onclick = () => {
+    product = products.filter(
+      (product) => product.name == productName.innerText
+    );
+    var prodOBJ = editBTN.parentNode.parentNode;
+    editProduct(prodOBJ, product);
+  };
 }
 
-//Event Listener On Changing The Amount of Product Using The Input
-prodAmount.addEventListener("input", () => {
-  let quantityNum = parseInt(prodAmount.value, 10);
-  let prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
-  let total = prodPriceVal * quantityNum;
-  productTotalPrice.innerHTML = total ? total : 0;
-});
 
-//Event Listener On Plus Button To Add 1 Product  to amount and update the total price
-plusBTN.onclick = () => {
-  var quantityNum = parseInt(prodAmount.value, 10);
-  let prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
 
-  quantityNum += 1;
-  prodAmount.value = quantityNum;
-  let total = prodPriceVal * quantityNum;
-  productTotalPrice.innerHTML = total ? total : 0;
-  console.log("Prod Amount", quantityNum);
-};
 
-//Event Listener On Minus Button To decrease 1 Product  from the  amount and update the total price
-minusBTN.onclick = () => {
-  var quantityNum = parseInt(prodAmount.value, 10);
-  let prodPriceVal = parseInt(productTitlePrice.innerHTML, 10);
 
-  quantityNum -= 1;
-  quantityNum = quantityNum > 0 ? quantityNum : 0;
-  prodAmount.value = quantityNum > 0 ? quantityNum : 0;
-  let total = prodPriceVal * quantityNum;
-  productTotalPrice.innerHTML = total > 0 ? total : 0;
-};
 
-//Event Listener On Delete Button To Delete 1 Product  from the  Products List and HTML
-deleteBTN.onclick = () => {
-  var productId;
-  prod = products.filter((product) => product.name == productName.innerText);
-  productId = prod[0].id;
 
-  deleteProduct(productId);
-  init(); // Load All Page Func Again
-};
 
-// Delete Product From Server
-const deleteProduct = async (productId) => {
+  // Delete Product From Server
+  const deleteProduct = async (productId) => {
     console.log(productId);
     await axios.delete(`/products/${productId}`).then((r) => r.data);
     console.log("Deleted");
   };
+
+
+// Edit Product From Server, ID Cannot Be Changed
+const editProduct = async (prodOBJ, product) => {
+    console.log("Editing Task");
+    console.log("ProdOBJ", prodOBJ);
+    console.log("Product", product);
+    name = prompt("Choose New Product Name")
+    price = prompt("Choose New product Price") 
+    amount = prompt("Choose New Product Amount")
+  
+    // Need To Add Not Finished Edit Mode
+    /**
+     * let containsClass = prodOBJ.classList.contains("editMode");
+     * if (!containsClass) {
+     * prodOBJ.classList.add("editMode");
+     * event.target.innerHTML = "Save";}
+     * else {
+     * prodOBJ.classList.remove("editMode");
+     * }
+    */
+    console.log(product[0].name)
+    var name = (name.length > 2) ? name:product[0].name
+    var price = (price > 0) ? price:product[0].price
+    var amount = (amount > 0) ? amount:product[0].amount
+    console.log(name, price ,amount)
+    console.log(parent)
+    var editProduct = new Product (product[0].id, name, price, amount, id_parent)
+    await axios.put(`/products/${product[0].id}`, editProduct).then((r) => r.data);
+    init();
+  }
   
