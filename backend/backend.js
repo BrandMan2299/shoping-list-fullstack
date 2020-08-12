0add27baa47bc33ddfcd0a7de5e9c32eba3a18ea
MrMoshkovitz <gal@gmail.com>
2020-08-12 11:00:49 +0300

    Finished Backend Fixing

--- backend/backend.js
+++ backend/backend.js
@@ -5,88 +5,82 @@ var path = require("path");
 app.use(express.json());
 
 let products = [
-    {
-      id: "0",
-      name: "milk",
-      price: 10,
-      amount: 0,
-    },
-    {
-      id: "1",
-      name: "cerial",
-      price: 23,
-      amount: 0,
-    },
-    {
-      id: "2",
-      name: "watermellon",
-      price: 29,
-      amount: 0,
-    },
-    {
-      id: "3",
-      name: "cola",
-      price: 12,
-      amount: 0,
-    },
-    {
-      id: "4",
-      name: "bread",
-      price: 8,
-      amount: 0,
-    },
-  ];
+  {
+    id: "0",
+    name: "milk",
+    price: 10,
+    amount: 0,
+  },
+  {
+    id: "1",
+    name: "cerial",
+    price: 23,
+    amount: 0,
+  },
+  {
+    id: "2",
+    name: "watermellon",
+    price: 29,
+    amount: 0,
+  },
+  {
+    id: "3",
+    name: "cola",
+    price: 12,
+    amount: 0,
+  },
+  {
+    id: "4",
+    name: "bread",
+    price: 8,
+    amount: 0,
+  },
+];
 
+app.get("/", (req, res) => {
+  app.use("/", express.static(path.join(__dirname, "../frontend")));
+  res.sendFile("home.html", { root: path.join(__dirname, "../frontend") });
+  // res.sendFile(`./../frontend/home.html`);
+});
 
+app.get("/products", (req, res) => {
+  res.send(products);
+});
 
-  app.get("/", (req, res) => {
-    app.use("/", express.static(path.join(__dirname, "../frontend")));
-    res.sendFile("home.html", { root: path.join(__dirname, "../frontend") });
-    // res.sendFile(`./../frontend/home.html`);
-  });
+app.get("/products/:id", (req, res) => {
+  for (const product of products) {
+    if (product.id === req.params.id) {
+      res.send(product);
+    }
+  }
+});
 
+app.post("/product", (req, res) => {
+  products.push(req.body);
+  console.log("Product", req.body);
+  res.send(req.body);
+});
 
-  
-  app.get("/products", (req, res) => {
-    res.send(products);
-  });
-
-  app.get("/products/:id", (req, res) => {
-    for (const product of products) {
-      if (product.id === req.params.id) {
-        res.send(product);
-      }
+app.put("/products/:id", (req, res) => {
+  products.forEach((product, index) => {
+    if (product.id === req.params.id) {
+      products.splice(index, 1, req.body);
+      console.log("New Product", req.body, "Old Product", product);
+      res.send(req.body);
     }
   });
+});
 
-  app.post("/product", (req, res) => {
-    products.push(req.body);
-    console.log("Product", req.body);
-    res.send(req.body);
+app.delete("/products/:id", (req, res) => {
+  products.forEach((product, index) => {
+    if (product.id == req.params.id) {
+      products.splice(index, 1);
+      console.log("Product", req.params.id, "Deleted");
+      res.send(`Product ${req.params.id} deleted`);
+    }
   });
-
-  app.put("/products/:id", (req, res) => {
-    products.forEach((product, index) => {
-      if (product.id === req.params.id) {
-        products.splice(index, 1, req.body);
-        console.log("New Product", req.body, "Old Product", product);
-        res.send(req.body);
-      }
-    });
-  });
-
-  app.delete("/products/:id", (req, res) => {
-    products.forEach((product, index) => {
-      if (product.id == req.params.id) {
-
-        products.splice(index, 1);
-        console.log("Product", req.params.id, "Deleted");
-        res.send(`Product ${req.params.id} deleted`);
-        
-      }
-    });
-  });
+});
 
 app.listen(port, () => {
-    console.log(`Server Is Listening On Port ${port}`);
-  });
+  console.log(`Server Is Listening On Port ${port}`);
+});
