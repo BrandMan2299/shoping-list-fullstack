50deb668118fdc8dbc2898d638210c7a7540c3f5
MrMoshkovitz <gal@gmail.com>
2020-08-16 07:09:54 +0300

    Integrating CSS That hasent Work

--- frontend/product.js
+++ frontend/product.js
@@ -4,7 +4,6 @@ const likeIcon = `<ion-icon name="heart-outline"></ion-icon>`;
 const minusIcon = `<ion-icon name="remove-circle-outline"></ion-icon>`;
 const plusIcon = `<ion-icon name="add-circle-outline"></ion-icon>`;
 const editIcon = `<i class="fal fa-edit"></i>`;
-
 // Product Object Creation
 function Product(_id, _name, _price, _amount, _parent) {
   this.id = _id;
